7270a6850f5b737fbc3147ed2c68eb34461d128e
Vadym Yakovlev <vadym_yakovlev@epam.com>
2017-08-26 09:26:05 -0700

    misc

--- src/main/java/my/poc/deeplearning4j/misc/NameExtractor.java
+++ src/main/java/my/poc/deeplearning4j/misc/NameExtractor.java
@@ -1,15 +1,17 @@
 package my.poc.deeplearning4j.misc;
 
-import java.io.File;
-import java.net.URL;
-import java.util.List;
 import org.deeplearning4j.models.word2vec.Word2Vec;
 import org.deeplearning4j.text.sentenceiterator.LineSentenceIterator;
 import org.deeplearning4j.text.sentenceiterator.SentenceIterator;
 import org.deeplearning4j.text.tokenization.tokenizer.preprocessor.CommonPreprocessor;
 import org.deeplearning4j.text.tokenization.tokenizerfactory.DefaultTokenizerFactory;
 import org.deeplearning4j.text.tokenization.tokenizerfactory.TokenizerFactory;
+import org.deeplearning4j.ui.UiServer;
 import org.junit.Test;
+
+import java.io.File;
+import java.net.URL;
+import java.util.List;
 
 /**
  *
@@ -40,7 +42,17 @@ public class NameExtractor {
 
                 vec.fit();
 
+                UiServer server = UiServer.getInstance();
+                System.out.println("Started on port " + server.getPort());
 
+                //ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+                //WordVectorSerializer.writeWord2VecModel(vec, byteArrayOutputStream);
+
+
+
+
+                System.out.println(vec);
             }
 
 
